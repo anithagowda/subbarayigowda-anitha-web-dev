62036a826c8e2b8706c75f25521d684936e6b4d0
asubbarayigowda <asubbarayigowda@massmutual.com>
2016-05-17 17:54:09 -0400

    Javascript to handle new and edit website actions

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -2,14 +2,29 @@
  * Created by asubbarayigowda on 5/17/16.
  */
 angular
-    .module("Website_list", [])
-    .controller("Website_list_controller", Website_list_controller);
+    .module("Websites", [])
+    .controller("Website_list_controller", Website_list_controller)
+    .controller("Website_edit_controller", Website_edit_controller)
+    .controller("Website_new_controller", Website_new_controller);
+
+website_list = [
+    {name:"Blogging App", description:"abcd"},
+    {name:"Address Book App", description:"abcd"},
+    {name:"Script Testing App", description:"abcd"},
+    {name:"Blogger", description:"Blogger is a blog publishing service"}];
 
 function Website_list_controller($scope) {
-        $scope.websites = [
-            "Blogging App",
-            "Address Book App",
-            "Script Testing App",
-            "Blogger"
-        ];
+        $scope.websites = website_list;
+}
+
+function Website_edit_controller($scope) {
+    $scope.selected_website = website_list[3];
+}
+
+function Website_new_controller($scope) {
+    $scope.createNewWebsite = function (name, description) {
+        var newWebsite = {name:name, description:description};
+        website_list.push(newWebsite);
+        console.log(website_list);
+    }
 }
