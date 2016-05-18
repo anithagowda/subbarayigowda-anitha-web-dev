48906c2187b759d3bb9c581ff791b6d6bb398956
asubbarayigowda <asubbarayigowda@massmutual.com>
2016-05-17 20:18:38 -0400

    Page pages

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,30 +1,46 @@
 /**
  * Created by asubbarayigowda on 5/17/16.
  */
-angular
-    .module("Websites", [])
-    .controller("Website_list_controller", Website_list_controller)
-    .controller("Website_edit_controller", Website_edit_controller)
-    .controller("Website_new_controller", Website_new_controller);
+(function() {
+    angular
+        .module("Websites", [])
+        .controller("Website_list_controller", Website_list_controller)
+        .controller("Website_edit_controller", Website_edit_controller)
+        .controller("Website_new_controller", Website_new_controller)
+        .controller("Page_list_controller", Page_list_controller);
 
-website_list = [
-    {name:"Blogging App", description:"abcd"},
-    {name:"Address Book App", description:"abcd"},
-    {name:"Script Testing App", description:"abcd"},
-    {name:"Blogger", description:"Blogger is a blog publishing service"}];
 
-function Website_list_controller($scope) {
+    website_list = [
+        {name: "Blogging App", description: "abcd"},
+        {name: "Address Book App", description: "abcd"},
+        {name: "Script Testing App", description: "abcd"},
+        {name: "Blogger", description: "Blogger is a blog publishing service"}];
+
+    function Website_list_controller($scope) {
         $scope.websites = website_list;
-}
-
-function Website_edit_controller($scope) {
-    $scope.selected_website = website_list[3];
-}
-
-function Website_new_controller($scope) {
-    $scope.createNewWebsite = function (name, description) {
-        var newWebsite = {name:name, description:description};
-        website_list.push(newWebsite);
-        console.log(website_list);
     }
-}
+
+    function Website_edit_controller($scope) {
+        $scope.selected_website = website_list[3];
+    }
+
+    function Website_new_controller($scope) {
+        $scope.createNewWebsite = function (name, description) {
+            var newWebsite = {name: name, description: description};
+            website_list.push(newWebsite);
+            console.log(website_list);
+        }
+    }
+
+    page_list = [
+        {name: "Blog Post", title: "abcd"},
+        {name: "Blogs", title: "abcd"},
+        {name: "Home", title: "abcd"},
+        {name: "About", title: "abcd"},
+        {name: "Contact Us", title: "abcd"}];
+    
+    function Page_list_controller($scope) {
+        $scope.pages = page_list;
+    }
+    
+})();
