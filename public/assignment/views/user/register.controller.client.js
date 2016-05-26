0b926822d3b19d429bda0e3ce61a85b56ddf80ee
asubbarayigowda <asubbarayigowda@massmutual.com>
2016-05-26 00:27:10 -0400

    Register with client and server

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -1,3 +1,31 @@
 /**
  * Created by asubbarayigowda on 5/25/16.
  */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("RegisterController", RegisterController);
+    
+    function RegisterController($location, UserService) {
+        var vm = this;
+        vm.register = register;
+
+        function register(username, password, verify_password) {
+            var user = UserService.findUserByUsername(username);
+            if (user === -1) {
+                if (password === verify_password) {
+                    var id = (new Date).getTime().toString();
+                    var newUser = {_id:id, username:username, password:password };
+                    UserService.createUser(newUser);
+                    $location.url("/user/"+id);
+                }
+                else {
+                    vm.error = "Password doesn't match!"
+                }
+            }
+            else {
+                vm.error = "Username already in use!"
+            }
+        }
+    }
+})();
