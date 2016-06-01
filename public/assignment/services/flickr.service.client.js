c9c3931193f581d611b37fd59c442445216710bf
asubbarayigowda <asubbarayigowda@massmutual.com>
2016-06-01 10:42:42 -0400

    Flickr service to search photos and select it

--- public/assignment/services/flickr.service.client.js
+++ public/assignment/services/flickr.service.client.js
@@ -8,8 +8,8 @@
 
     function FlickrService($http) {
 
-        var key = "your-flickr-key";
-        var secret = "your-flickr-secret";
+        var key = "91d2ce699f5583eb5f29d3d52e357492";
+        var secret = "44a6aae5b5be8e27";
         var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
 
         var apis = {
@@ -18,10 +18,10 @@
 
         function searchPhotos(searchText) {
             console.log("Server " + searchText);
-            // var url = urlBase
-            //     .replace("API_KEY", key)
-            //     .replace("TEXT", searchText);
-            // return $http.get(url);
+            var url = urlBase
+                .replace("API_KEY", key)
+                .replace("TEXT", searchText);
+            return $http.get(url);
         }
 
         return apis;
--- public/assignment/views/widget/widget_flickr_search.controller.client.js
+++ public/assignment/views/widget/widget_flickr_search.controller.client.js
@@ -6,13 +6,32 @@
         .module("WebAppMaker")
         .controller("FlickrImageSearchController", FlickrImageSearchController);
 
-    function FlickrImageSearchController(FlickrService) {
+    function FlickrImageSearchController(FlickrService, WidgetService) {
         var vm = this;
+
         vm.searchPhotos = searchPhotos;
+        vm.selectPhoto = selectPhoto;
 
         function searchPhotos(searchText) {
-            console.log(searchText);
-            FlickrService.searchPhotos(searchText);
+            FlickrService
+                .searchPhotos(searchText)
+                .then(function (res) {
+                    var data = res.data.replace("jsonFlickrApi(","");
+                    data = data.substring(0, data.length - 1);
+                    data = JSON.parse(data);
+                    vm.photos = data.photos;
+                });
+        }
+
+        function selectPhoto(photo) {
+            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_b.jpg";
+            console.log(url);
+            // var newWidget = { "widgetType": "IMAGE", "pageId": pageId, "width": 100%, "url": url};
+            // WidgetService
+            //     .updateWidget(wgid, newWidget)
+            //     .then(function (res) {
+            //
+            //     });
         }
     }
 })();
