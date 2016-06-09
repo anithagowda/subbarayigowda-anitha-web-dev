bbf047a8dc5a7fe5a3daf60d98495cbe41778a61
asubbarayigowda <asubbarayigowda@massmutual.com>
2016-06-08 20:56:50 -0400

    Changed mongoose connect as it is not working in Openshift

--- public/assignment/model/models.server.js
+++ public/assignment/model/models.server.js
@@ -4,8 +4,22 @@
 module.exports = function () {
 
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/cs5610WebAppMaker');
+    // mongoose.connect('mongodb://localhost/cs5610WebAppMaker');
 
+
+    var connect = function () {
+        mongoose.connect('mongodb://localhost/cs5610WebAppMaker');
+    };
+    connect();
+
+    var db = mongoose.connection;
+
+    db.on('error', function(error){
+        console.log("Error loading the db - "+ error);
+    });
+
+    db.on('disconnected', connect);
+    
     var models = {
         userModel: require("./user/user.model.server")(),
         websiteModel: require("./website/website.model.server")(),
